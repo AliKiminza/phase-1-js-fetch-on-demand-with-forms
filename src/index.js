ce3a518e1845921dd5dd61cbaf37ccf7d4a22d8e
alikiminza <ali.kiminza@student.moringaschool.com>
2023-06-29 08:41:45 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector ("form")
+  inputForm.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    
+    const input = document.querySelector('input#searchByID')
 
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then (response => response.json())
+    .then (data => {
+      const tittle = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+
+      tittle.innerText = data.tittle;
+      summary.innerText = data.summary;
+  
+    });
+
+  });
+
+
+}
 document.addEventListener('DOMContentLoaded', init);
